6bd1c6af6a821361037ba85eff318d93fc126147
quanglam04 <trinhquanglam2k4@gmail.com>
2025-10-24 22:34:01 +0700

    Bài tập phần 1(1/2)

---
+++ lamtq_b22dccn482_training_gd1/Tuần 1/Phần 1/04_DCL_DATA_CONTROL/02-User-Permission.sql
@@ -0,0 +1,16 @@
+1. GRANT 
+
+GRANT SELECT ON SINH_VIEN TO 'lecturer'@'localhost';
+- Mục đích: Cấp quyền đọc (SELECT) trên bảng SINH_VIEN cho người dùng có tên là lecturer khi họ kết nối từ máy chủ localhost 
+
+GRANT INSERT, UPDATE, DELETE ON DIEM_SO TO 'admin'@'%';
+- Mục đích: Cấp quyền chèn, cập nhật, xóa (INSERT, UPDATE, DELETE) trên bảng DIEM_SO cho người dùng admin kết nối từ bất kỳ đâu (%)
+
+
+2. REVOKE 
+
+REVOKE DELETE ON DIEM_SO FROM 'lecturer'@'localhost';
+- Mục đích: Thu hồi quyền xóa (DELETE) trên bảng DIEM_SO khỏi người dùng lecturer 
+
+REVOKE ALL PRIVILEGES ON MON_HOC FROM 'admin'@'%';
+- Mục đích: Thu hồi tất cả quyền (ALL PRIVILEGES) trên bảng MON_HOC khỏi người dùng admin 
